db6542e01d5fca88b76686324de9efd696669559
Manu Finn <Manu_Finn117@outlook.com>
2021-09-25 11:28:20 -0500

    Agregar archivos de proyecto.

---
+++ U01A02a181G0250/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using U01A02a181G0250.Models;
+
+namespace U01A02a181G0250.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index( CalificacionesViewModel vm)
+        {
+            return View(vm);
+        }
+
+        public IActionResult CalificacionFinal (int Calif01, int Calif02, int Calif03)
+        {
+            return Ok((Calif01 + Calif02 + Calif03) / 3);
+        }
+    }
+}
---
+++ U01A02a181G0250/Models/CalificacionesViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace U01A02a181G0250.Models
+{
+    public class CalificacionesViewModel
+    {
+        public int Calificacion01 { get; set; }
+        public int Calificacion02 { get; set; }
+        public int Calificacion03 { get; set; }
+
+        public int CalificacionFinal { 
+            get { return (Calificacion01 + Calificacion02 + Calificacion03) / 3; } 
+        }
+
+        public string Estado { 
+            get
+            {
+                if(CalificacionFinal >= 70) { return "Aprobado"; }
+                if(CalificacionFinal < 70) { return "Reprobado"; }
+                return " ";
+            }
+        }
+    }
+}
